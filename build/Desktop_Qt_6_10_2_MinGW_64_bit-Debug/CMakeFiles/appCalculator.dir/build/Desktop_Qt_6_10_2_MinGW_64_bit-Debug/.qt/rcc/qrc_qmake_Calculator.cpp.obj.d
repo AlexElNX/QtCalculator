f79680ad23510205e44df7fba128f3c7e826a6fa
Alex <stdcpp@gmail.com>
2026-05-23 13:43:08 -0500

    Remove build and qtcreator files from repo

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/CMakeFiles/appCalculator.dir/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/rcc/qrc_qmake_Calculator.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/appCalculator.dir/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/rcc/qrc_qmake_Calculator.cpp.obj: \
- D:\Programming\C++\ hillel\Calculator\build\Desktop_Qt_6_10_2_MinGW_64_bit-Debug\.qt\rcc\qrc_qmake_Calculator.cpp
--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/CMakeFiles/appCalculator_qmllint.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/appCalculator_qmllint"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/appCalculator_qmllint.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
